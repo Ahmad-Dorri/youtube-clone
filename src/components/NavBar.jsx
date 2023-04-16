d20d665c4d90b3ae492ef16c3b7c9ad62000117d
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-04-16 16:56:45 +0430

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { logo } from '../utils/constanst';
+import SearchBar from './SearchBar';
+const NavBar = () => {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        backgroundColor: '#000',
+        justifyContent: 'space-between',
+        position: 'sticky',
+        top: 0,
+      }}>
+      <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
+    </Stack>
+  );
+};
+
+export default NavBar;
