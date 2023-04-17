46c04385a7970627c0a3f9ecd293d49b07b29484
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-04-17 14:45:22 +0430

    adding SearchBar.jsx component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,23 @@
-
-import React from 'react';
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import { Search } from '@mui/icons-material';
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [searchTerm, setSearchTerm] = useState('');
+  const inputChangeHandler = (event) => {
+    setSearchTerm(event.target.value)
+  }
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{ borderRadius: 20, border: '1px solid #e3e3e3',pl: 2, mr: {sm: 5}, boxShadow: 'none' }}>
+      <input placeholder='Search...' value={searchTerm} onChange={inputChangeHandler} className='search-bar'  />
+      <IconButton type='submit' sx={{color: 'red', p: '10px'}}>
+        <Search/>
+      </IconButton>
+    </Paper>
+  );
 };
 
 export default SearchBar;
